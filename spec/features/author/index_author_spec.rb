f26b587e5d9854e9bc7152e2bc2b95ec3a8692de
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-11-20 20:35:21 +0100

    Add link to new author

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -8,8 +8,9 @@ describe "Index author page", type: :feature do
     end
 
     it "should have table entry for an author's first name, last name, and homepage" do
-        # visit authors_path
-        
+        visit authors_path
+        expect(page).to have_link 'New', href: new_author_path
+
         # expect(page).to have_text("Jemaine")
         # expect(page).to have_text("Clement")
         # expect(page).to have_text("flight_of_the_conchords.com")
