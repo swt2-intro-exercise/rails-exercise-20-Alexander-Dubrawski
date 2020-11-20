c9a005976b5bbe66a0be3815379f90f3105cac15
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-11-20 17:43:58 +0100

    Implment add new author

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  post '/author/new' , to: 'author#create'
   get '/author/new', to: 'author#new'
   get '/new_author' , to: 'author#new'
 
