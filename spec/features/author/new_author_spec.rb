096cc3693432eafc7dbb7c347bf3a19267069225
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-11-20 17:34:46 +0100

    Add test for user creation

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
-    before(:each) do
-        @author = Author.new(first_name: "Bret",last_name: "McKenzie", homepage: "https://en.wikipedia.org/wiki/Flight_of_the_Conchords")
-    end
  
     it "should render withour error" do
         visit new_author_path
@@ -16,4 +13,14 @@ describe "New author page", type: :feature do
         expect(page).to have_field('author[last_name]')
         expect(page).to have_field('author[homepage]')
     end
+
+    it "should create new author" do 
+        visit new_author_path
+        page.fill_in 'author[first_name]', with: 'Bret'
+        page.fill_in 'author[last_name]', with: 'McKenzie'
+        page.fill_in 'author[homepage]', with: 'https://en.wikipedia.org/wiki/Flight_of_the_Conchords'
+        find('input[type="submit"]').click
+        expect(Author.count).to eq(1)
+    end 
+
 end
