6cb78fb0ae066ab0decc05623afc4c78e9de5f97
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-11-20 13:40:46 +0100

    Add new author controller and route

---
+++ app/controllers/new_author_controller.rb
@@ -0,0 +1,4 @@
+class NewAuthorController < ApplicationController
+    def new
+    end
+end
---
+++ spec/controllers/new_author_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe NewAuthorController, type: :controller do
+
+end
