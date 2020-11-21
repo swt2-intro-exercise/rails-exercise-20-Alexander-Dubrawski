65e802fbc215ff68a5e4f5dd83b0aa03ab3b5899
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-11-21 17:57:37 +0100

    Here you go travis

--- db/migrate/20201121155034_create_join_table_author_paper.rb
+++ db/migrate/20201121155034_create_join_table_author_paper.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableAuthorPaper < ActiveRecord::Migration[6.0]
   def change
     create_join_table :authors, :papers do |t|
-      t.belongs_to :author
-      t.belongs_to :paper
+      t.index [:paper_id, :author_id]
+      t.index [:author_id, :paper_id]
     end
   end
 end
