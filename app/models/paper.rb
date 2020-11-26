1486616068290885dffd0afed980efc0b226c06d
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-11-26 16:59:44 +0100

    Add scope for paper

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,6 @@
 class Paper < ApplicationRecord
     has_and_belongs_to_many :authors
+    scope :published,  -> (n) { where("year = ?", n) }
     validates :title, presence: true, allow_blank: false
     validates :venue, presence: true, allow_blank: false
     validates :year, presence: true, numericality: true
